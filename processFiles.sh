a98fcc0a5c6c43f988739c9c43100e83446719df
Minna Tamper <minna.tamper@aalto.fi>
2016-02-09 11:22:19 +0200

    Ducomenting scripts

--- processFiles.sh
+++ processFiles.sh
@@ -2,17 +2,23 @@
 FILES=/home/minna/kansaTaisteliLehdet/*.pdf
 for f in $FILES
 do
+  #get filenames
   fNameFirst=${f##*/}
   fName=${fNameFirst%.*}
   ocr=fName
+
   echo "Processing $fName file..."
+
+  #access file directory (assuming that input is in a folder with a corresponding name)
   DIR="DIR_$fName"
   mkdir $DIR
   cp $f $DIR/
   cd $DIR
+
+  #convert pdf to an images (one page, one image)
   gs -dNOPAUSE -sDEVICE=jpeg -dFirstPage=1 -sOutputFile=output%d.jpg -dJPEGQ=100 -r500 -q $fNameFirst -c quit
-  #tesseract output%d.jpg $ocr%d.txt -l fin 
+
+  #execute tesseract
   for i in $(ls -v *.jpg) ; do echo "$i $i.txt" ; tesseract $i $i.txt -l fin; done
-  #cat $f
   cd ..
 done
